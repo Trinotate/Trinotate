7d95f3e85cb651f3f111683f62f36e7d879783c2
Brian Haas <brianjohnhaas@gmail.com>
2015-04-15 19:39:49 -0400

    update

--- admin/cleanMe.pl
+++ admin/cleanMe.pl
@@ -2,12 +2,22 @@
 
 use strict;
 
-my @keep = qw(cleanMe.pl
-              EMBL_dat_to_Trinotate_sqlite_resourceDB.pl
-              RunMeAdminTest.pl
-              notes
+my @keep = qw(
 EMBL_dat_parser.pl
+EMBL_dat_retrieve_via_id_list.pl
+EMBL_dat_to_Trinotate_sqlite_resourceDB.pl
+EMBL_sprot_n_uniref90_parser.pl
 PFAM_dat_parser.pl
+PFAMtoGoParser.pl
+RunMeAdminTest.pl
+TaxonomyUniq
+Taxonomyvalue.pre
+Trinotate.sqlite
+cleanMe.pl
+go.obo.sample.tab
+init_Trinotate_sqlite_db.pl
+notes
+testData/
 
 
 
