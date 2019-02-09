409b56df1691079f2bf96a25df99f6948722e5c2
Brian Haas <brianjohnhaas@gmail.com>
2019-02-09 09:47:30 -0500

    updated the pfam2go url

--- admin/Build_Trinotate_Boilerplate_SQLite_db.pl
+++ admin/Build_Trinotate_Boilerplate_SQLite_db.pl
@@ -21,7 +21,8 @@ my $SPROT_DAT_URL = "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release
 my $EGGNOG_DAT_URL = "http://eggnogdb.embl.de/download/latest/data/NOG/NOG.annotations.tsv.gz";
 my $GENE_ONTOLOGY_DAT_URL = "http://purl.obolibrary.org/obo/go/go-basic.obo";
 my $PFAM_DAT_URL = "ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz";
-my $PFAM2GO_DAT_URL = "http://www.geneontology.org/external2go/pfam2go"; # ftp://ftp.geneontology.org/pub/go/external2go/pfam2go 
+#my $PFAM2GO_DAT_URL = "http://www.geneontology.org/external2go/pfam2go"; # ftp://ftp.geneontology.org/pub/go/external2go/pfam2go
+my $PFAM2GO_DAT_URL = "ftp://ftp.geneontology.org/pub/go/external2go/pfam2go";
 
 
 main: {
