d7f757cbc2ab1163fc0453cc8121e896b43ef013
Ramon Rivera <riveravicens@gmail.com>
2021-03-24 20:33:49 -0700

    New pfam2go url

--- admin/Build_Trinotate_Boilerplate_SQLite_db.pl
+++ admin/Build_Trinotate_Boilerplate_SQLite_db.pl
@@ -22,7 +22,7 @@ my $EGGNOG_DAT_URL = "http://eggnogdb.embl.de/download/eggnog_4.5/data/NOG/NOG.a
 my $GENE_ONTOLOGY_DAT_URL = "http://purl.obolibrary.org/obo/go/go-basic.obo";
 my $PFAM_DAT_URL = "ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz";
 #my $PFAM2GO_DAT_URL = "http://www.geneontology.org/external2go/pfam2go"; # ftp://ftp.geneontology.org/pub/go/external2go/pfam2go
-my $PFAM2GO_DAT_URL = "ftp://ftp.geneontology.org/pub/go/external2go/pfam2go";
+my $PFAM2GO_DAT_URL = "http://current.geneontology.org/ontology/external2go/pfam2go";
 
 
 main: {
