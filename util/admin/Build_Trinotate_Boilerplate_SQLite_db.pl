f747ef1e6587f51e495e2d573cc45ce450a1b632
Brian Haas <brianjohnhaas@gmail.com>
2023-01-24 21:58:55 +0000

    adjustment to checkpoint dir specification

--- util/admin/Build_Trinotate_Boilerplate_SQLite_db.pl
+++ util/admin/Build_Trinotate_Boilerplate_SQLite_db.pl
@@ -36,56 +36,59 @@ main: {
     }
     
 
-    my $pipeliner = new Pipeliner(-verbose => 2);
-
     unless (-d $checkpoint_dir) {
         mkdir $checkpoint_dir or die "Error, cannot mkdir $checkpoint_dir";
     }
+    
+    
+    my $pipeliner = new Pipeliner(-verbose => 2,
+                                  -checkpoint_dir => $checkpoint_dir);
+    
 
     ## process Sprot dat file
-    $pipeliner->add_commands(new Command("wget \"$SPROT_DAT_URL\"", "$checkpoint_dir/wget_sprot_dat.ok") );
+    $pipeliner->add_commands(new Command("wget \"$SPROT_DAT_URL\"", "wget_sprot_dat.ok") );
 
-    $pipeliner->add_commands(new Command("$UTILDIR/EMBL_swissprot_parser.pl uniprot_sprot.dat.gz $prefix", "$checkpoint_dir/parse_sprot_dat.ok"));
+    $pipeliner->add_commands(new Command("$UTILDIR/EMBL_swissprot_parser.pl uniprot_sprot.dat.gz $prefix", "parse_sprot_dat.ok"));
     
     $pipeliner->add_commands(new Command("mv uniprot_sprot.dat.gz.pep uniprot_sprot.pep",
-                                         "$checkpoint_dir/rename_sprot_pep_file.ok"));
+                                         "rename_sprot_pep_file.ok"));
     
     
 
     # create sqlite database and load in the swissprot data:
 
-    $pipeliner->add_commands(new Command("$UTILDIR/EMBL_dat_to_Trinotate_sqlite_resourceDB.pl --sqlite $sqlite_db --create", "$checkpoint_dir/init_sqlite_db.ok"));
+    $pipeliner->add_commands(new Command("$UTILDIR/EMBL_dat_to_Trinotate_sqlite_resourceDB.pl --sqlite $sqlite_db --create", "init_sqlite_db.ok"));
 
     $pipeliner->add_commands(new Command("$UTILDIR/EMBL_dat_to_Trinotate_sqlite_resourceDB.pl --sqlite $sqlite_db --uniprot_index $prefix.UniprotIndex",
-                             "$checkpoint_dir/uniprot_index_loading.ok") );
+                             "uniprot_index_loading.ok") );
 
     $pipeliner->add_commands(new Command("$UTILDIR/EMBL_dat_to_Trinotate_sqlite_resourceDB.pl --sqlite $sqlite_db --taxonomy_index $prefix.TaxonomyIndex",
-                             "$checkpoint_dir/taxonomy_index_loading.ok") );
+                             "taxonomy_index_loading.ok") );
     
 
     ##########
     ## EGGNOG
-    $pipeliner->add_commands(new Command("wget \"$EGGNOG_DAT_URL\"", "$checkpoint_dir/eggnog_download.ok") );
+    $pipeliner->add_commands(new Command("wget \"$EGGNOG_DAT_URL\"", "eggnog_download.ok") );
     
     # extract fields
     $pipeliner->add_commands(new Command("gunzip -c NOG.annotations.tsv.gz | $UTILDIR/print.pl 1 5 > NOG.annotations.tsv.gz.bulk_load",
-                                         "$checkpoint_dir/eggnog_field_extraction.ok") );  # note, had set -eou pipefail, but this generated errors on certain flavors and/or versions of linux
+                                         "eggnog_field_extraction.ok") );  # note, had set -eou pipefail, but this generated errors on certain flavors and/or versions of linux
     # load 
     $pipeliner->add_commands(new Command("$UTILDIR/EMBL_dat_to_Trinotate_sqlite_resourceDB.pl --sqlite $sqlite_db --eggnog NOG.annotations.tsv.gz.bulk_load",
-                                         "$checkpoint_dir/eggnog.load.ok") );
+                                         "eggnog.load.ok") );
     
     
     
     ################
     ## Gene ontology
 
-    $pipeliner->add_commands(new Command("wget \"$GENE_ONTOLOGY_DAT_URL\"", "$checkpoint_dir/go_download.ok"));
+    $pipeliner->add_commands(new Command("wget \"$GENE_ONTOLOGY_DAT_URL\"", "go_download.ok"));
 
     $pipeliner->add_commands(new Command("$UTILDIR/obo_to_tab.pl go-basic.obo > go-basic.obo.tab",
-                                         "$checkpoint_dir/go_obo_to_tab.ok"));
+                                         "go_obo_to_tab.ok"));
 
     $pipeliner->add_commands(new Command("$UTILDIR/EMBL_dat_to_Trinotate_sqlite_resourceDB.pl --sqlite $sqlite_db --go_obo_tab go-basic.obo.tab",
-                                         "$checkpoint_dir/go_obo_load.ok"));
+                                         "go_obo_load.ok"));
 
     
 
@@ -93,25 +96,25 @@ main: {
     ##############
     ## Pfam
 
-    $pipeliner->add_commands(new Command("wget \"$PFAM_DAT_URL\"", "$checkpoint_dir/download_pfam.ok"));
+    $pipeliner->add_commands(new Command("wget \"$PFAM_DAT_URL\"", "download_pfam.ok"));
     
-    $pipeliner->add_commands(new Command("$UTILDIR/PFAM_dat_parser.pl Pfam-A.hmm.gz", "$checkpoint_dir/pfam_parsing.ok"));
+    $pipeliner->add_commands(new Command("$UTILDIR/PFAM_dat_parser.pl Pfam-A.hmm.gz", "pfam_parsing.ok"));
     
     $pipeliner->add_commands(new Command("$UTILDIR/EMBL_dat_to_Trinotate_sqlite_resourceDB.pl --sqlite $sqlite_db --pfam Pfam-A.hmm.gz.pfam_sqlite_bulk_load",
-                                         "$checkpoint_dir/pfam_loading.ok") );
+                                         "pfam_loading.ok") );
     
     
     #############
     ## Pfam2Go
 
     $pipeliner->add_commands(new Command("wget \"$PFAM2GO_DAT_URL\" ",
-                                         "$checkpoint_dir/pfam2go_download.ok") );
+                                         "pfam2go_download.ok") );
 
     $pipeliner->add_commands(new Command("$UTILDIR/PFAMtoGoParser.pl pfam2go > pfam2go.tab",
-                                         "$checkpoint_dir/pfam2go_tab.ok"));
+                                         "pfam2go_tab.ok"));
 
     $pipeliner->add_commands(new Command("$UTILDIR/EMBL_dat_to_Trinotate_sqlite_resourceDB.pl --sqlite $sqlite_db --pfam2go pfam2go.tab",
-                                         "$checkpoint_dir/pfam2go_tab_loading.ok"));
+                                         "pfam2go_tab_loading.ok"));
 
     
     
