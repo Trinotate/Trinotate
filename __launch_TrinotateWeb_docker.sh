b4aeeb32c10c348f582b7500600ef792800f30b6
Brian Haas <brianjohnhaas@gmail.com>
2023-02-01 16:54:24 +0000

    added launchers of trinotateweb via docker or singularity

---
+++ __launch_TrinotateWeb_docker.sh
@@ -0,0 +1,1 @@
+docker run --rm -it -v `pwd`:/data -p 8088:8088  trinityrnaseq/trinotate /usr/local/src/Trinotate/run_TrinotateWebserver.pl 8088
---
+++ __launch_TrinotateWeb_singularity.sh
@@ -0,0 +1,1 @@
+singularity exec -e -B /tmp -B `pwd` Docker/trinotate.simg /usr/local/src/Trinotate/run_TrinotateWebserver.pl 8088
